277ecd32c5928a4ab7f3a06e69758fbb2e3e2f67
Edward Yang <yushine999@gmail.com>
2014-06-20 15:13:07 +0800

    re #670 update_columns only support rails 4.0 +

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -102,10 +102,8 @@ module Spree
     def update!(order)
       old_state = state
       new_state = determine_state(order)
-      update_columns(
-        state: new_state,
-        updated_at: Time.now,
-      )
+      update_column 'state', new_state
+      update_column 'updated_at', Time.now
       after_ship if new_state == 'shipped' and old_state != 'shipped'
     end
 

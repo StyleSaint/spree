9154080301a0a4e608ac37ea98a861e53a78f854
Baoliang Zhang <zhangbaoliang.star@gmail.com>
2015-08-28 10:19:35 +0800

    re #1955 add status filter

--- core/app/controllers/spree/admin/orders_controller.rb
+++ core/app/controllers/spree/admin/orders_controller.rb
@@ -6,7 +6,52 @@ module Spree
       before_filter :load_order, :only => [:show, :edit, :update, :fire, :resend]
 
       respond_to :html
-      
+
+      def index
+        params[:q] ||= {}
+        params[:q][:completed_at_not_null] ||= '1' if Spree::Config[:show_only_complete_orders_by_default]
+        @show_only_completed = params[:q][:completed_at_not_null].present?
+        params[:q][:s] ||= @show_only_completed ? 'completed_at desc' : 'created_at desc'
+
+        # As date params are deleted if @show_only_completed, store
+        # the original date so we can restore them into the params
+        # after the search
+        created_at_gt = params[:q][:created_at_gt]
+        created_at_lt = params[:q][:created_at_lt]
+
+        params[:q].delete(:inventory_units_shipment_id_null) if params[:q][:inventory_units_shipment_id_null] == "0"
+
+        if !params[:q][:created_at_gt].blank?
+          params[:q][:created_at_gt] = Time.zone.parse(params[:q][:created_at_gt]).beginning_of_day rescue ""
+        end
+
+        if !params[:q][:created_at_lt].blank?
+          params[:q][:created_at_lt] = Time.zone.parse(params[:q][:created_at_lt]).end_of_day rescue ""
+        end
+
+        if @show_only_completed
+          params[:q][:completed_at_gt] = params[:q].delete(:created_at_gt)
+          params[:q][:completed_at_lt] = params[:q].delete(:created_at_lt)
+        end
+
+        if !params[:q]['state_eq'].blank? and  params[:q]['state_eq'] == 'ready'
+          params[:q].delete 'state_eq'
+          @search = Spree::Order.where('`spree_shipments`.`state` = ? ', 'ready').accessible_by(current_ability, :index).ransack(params[:q])
+          params[:q]['state_eq'] = 'ready'
+        else
+          @search = Spree::Order.accessible_by(current_ability, :index).ransack(params[:q])
+        end
+        @orders = @search.result.includes([:user, :shipments, :payments]).
+          page(params[:page]).
+          per(params[:per_page] || Spree::Config[:orders_per_page])
+
+        # Restore dates
+        params[:q][:created_at_gt] = created_at_gt
+        params[:q][:created_at_lt] = created_at_lt
+
+        respond_with(@orders)
+      end
+
       def show
         respond_with(@order)
       end
